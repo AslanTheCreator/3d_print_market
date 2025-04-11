845096ec1b9e33cd2cb13c88e8c3ce56ffa67d26
Aslan <aslantupenov00@gmail.com>
2025-04-11 11:44:19 +0300

    feat(favorites): добавление новых методов api

--- src/entities/favorites/api/favoritesApi.ts
+++ src/entities/favorites/api/favoritesApi.ts
@@ -1,18 +1,43 @@
+import { ProductCardModel, ProductResponseModel } from "@/entities/product";
 import { createAuthenticatedAxiosInstance } from "@/shared/api/axios/authenticatedInstance";
 import config from "@/shared/config/api";
 import { errorHandler } from "@/shared/lib/errorHandler";
 
+const API_URL = `${config.apiBaseUrl}/favorites`;
+const authenticatedAxios = createAuthenticatedAxiosInstance();
+
 export const favoritesApi = {
+  getFavorites: async (): Promise<ProductCardModel[]> => {
+    try {
+      const { data } = await authenticatedAxios.post<ProductResponseModel>(
+        `${API_URL}/find`,
+        {}
+      );
+      return data.content;
+    } catch (error) {
+      throw errorHandler.handleAxiosError(
+        error,
+        "Ошибка при загрузке избранных товаров"
+      );
+    }
+  },
   addToFavorites: async (productId: number) => {
     try {
-      const authenticatedAxios = createAuthenticatedAxiosInstance();
-      await authenticatedAxios.post(
-        `${config.apiBaseUrl}/favorites?productId=${productId}`
-      );
+      await authenticatedAxios.post(`${API_URL}?productId=${productId}`);
     } catch (error) {
       throw errorHandler.handleAxiosError(
         error,
         "Ошибка при добавлении товара в избранное"
+      );
+    }
+  },
+  removeFromFavorites: async (productId: number) => {
+    try {
+      await authenticatedAxios.delete(`${API_URL}?productId=${productId}`);
+    } catch (error) {
+      throw errorHandler.handleAxiosError(
+        error,
+        "Ошибка при удалении товара из избранного"
       );
     }
   },
